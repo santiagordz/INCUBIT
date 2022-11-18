2c6ba3938a637058593088711fba86d525287c0c
Santiago Rodriguez Murialdo <67066757+santiagordz@users.noreply.github.com>
2022-11-18 10:47:21 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ echo "La temperatura es:" .$temperatura. "<br>La humedad es: " .$humedad;
 $usuario = "root";
 $contraseña = "";
 $servidor = "localhost";
-$database = "temphumidnew";
+$database = "incubit";
 
 $conexion = mysqli_connect($servidor, $usuario, $contraseña) or die ("No se puede conectar a la base de datos");
 $db = mysqli_select_db($conexion, $database) or die ("No se puede seleccionar la base de datos");
 
 
-$consulta = "INSERT INTO dht11(fecha, hora, temperature, humidity) VALUES (CURDATE(), CURTIME(), ".$temperatura.", ".$humedad.")";
+$consulta = "INSERT INTO medicion(fecha, hora, temperatura, humedad) VALUES (CURDATE(), CURTIME(), ".$temperatura.", ".$humedad.")";
 $resultado = mysqli_query($conexion, $consulta);
 
 ?>
